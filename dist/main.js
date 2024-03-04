429af59f4c6c2cbcb305735e1b9171ea6b8a6e4f
Daniel Oliva <doliva@adobe.com>
2024-03-04 10:15:38 -0700

    docs(mwpw-143651): links the right ticket number

--- dist/main.js
+++ dist/main.js
@@ -1,5 +1,5 @@
 /*!
- * Chimera UI Libraries - Build 0.11.11 (3/4/2024, 09:41:06)
+ * Chimera UI Libraries - Build 0.11.11 (3/4/2024, 10:15:45)
  *         
  */
 /******/ (function(modules) { // webpackBootstrap
